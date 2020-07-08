057a0a87bae0bc86765efa204e385bf59f456a91
maxwell_luong <maxwell.luong@nortonlifelock.com>
2020-07-08 16:59:56 -0700

    initial commit

---
+++ hello-world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello-world
+//
+//  Created by Maxwell Luong on 7/8/20.
+//  Copyright © 2020 QE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
